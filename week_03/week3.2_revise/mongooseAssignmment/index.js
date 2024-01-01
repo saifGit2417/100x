16feea214bc52188690b9615baf7a94e53c77b7b
saif <mdsaifurrahman@navneettoptech.com>
2024-01-01 16:02:02 +0530

    till 3.2 completed

--- week_03/week3.2_revise/mongooseAssignmment/index.js
+++ week_03/week3.2_revise/mongooseAssignmment/index.js
@@ -6,6 +6,7 @@ const portNumber = 3000;
 //  middle wares
 app.use(express.json());
 
+// connect with db using mongoose
 mongoose
   .connect(
     "mongodb+srv://saif1:PqKXa0gXCJevEoqf@cluster0.yjfo0fs.mongodb.net/100x_Db"
@@ -14,31 +15,40 @@ mongoose
     console.log("mongoose connected successfully");
   });
 
-const Users = mongoose.model("usersTable", {
+//  creating  schema explicitly for better code readability
+const UserSchema = new mongoose.Schema({
   userName: String,
   password: String,
   email: String,
 });
 
-app.post("/signup", async (req, res) => {
-  const userName = req.body.userName;
-  const password = req.body.password;
-  const email = req.body.email;
+// create model defining what would be collection name inside data base and define its schema that we made above this
+const Users = mongoose.model("usersTable", UserSchema);
 
+// creating end point to signup user
+app.post("/signup", async (req, res) => {
+  
+  // destructure req body instead of using req.body method for each thing
+  const { userName, password, email } = req.body;
+
+  // checking if any user with same email exists in our table
   let existingUser = await Users.findOne({ email });
 
+  // if user exists with same email return error , cant have more than one user with same email id
   if (existingUser) {
     return res.status(400).json({
       msg: `Email is already registered with us, please try a different email to register`,
     });
   }
 
+  // adding new user to Users collection
   const addUser = new Users({
     userName,
     password,
     email,
   });
 
+  // without doing .save it wont save entry in db
   addUser.save().then(() => {
     res.json("new entry added to DB");
   });
