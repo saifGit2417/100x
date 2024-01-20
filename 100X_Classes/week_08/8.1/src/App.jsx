02e94be3727118c6c54ed61ea4cddf2248c4961a
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-01-20 19:34:06 +0530

    8.1 about tailwind css

---
+++ 100X_Classes/week_08/8.1/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="flex ">
+      <div>hui</div>
+      <div>hui</div>
+      <div>hui</div>
+      <div>hui</div>
+      <div>hui</div>
+      <div>hui</div>
+      <div>hui</div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
