8ec92009a5cf6ad0f7a02b79f1e61d0b7053d0ea
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-04-27 17:13:42 +0530

    class 12.3 : notes and command added

--- 100X_Classes/week_12/week_12.3/index.js
+++ 100X_Classes/week_12/week_12.3/index.js
@@ -1,12 +1,32 @@
 // Import the pg library
 const { Client } = require("pg");
 
-
 // to run postgres sql in docker locally run bel0w command
 // any request coming to 5435 in my machine will go to 5432 to docker image running postgresql
 // docker run -e POSTGRES_PASSWORD=mysecretpassword -d -p 5435:5432 postgres
 
+/*
+below are common command to run docker
 
+any image/software you wanna run in docker search for its image name on docker hub and run command like below
+
+docker run image-name >>>>> docker run mongo
+if you want to ruyn docker in background then add -d after run this means detached mode
+
+docker run -d mongo
+
+==========PORT MAPPING===========
+how to make sure some request coming to your local machine will redirect to docker container
+for that we have to do port mapping .. below is the command for that
+docker run -d -p 27017:27017 mongo >>> docker run in detached mode i.e in background application 
+-p stands for port mapping any request coming to 27017 to my local will get redirect to 27017 to my docker image that run mongo image
+
+=========FIND HOW MANY IMAGE RUNNING IN MY DOCKER LOCALLY============
+docker ps >>>>>>>> will give you the list of all images running locally
+docker kill image_id >>> it wil that particular image runing on docker locally
+
+
+*/
 
 // Define your connection string (replace placeholders with your actual data)
 const connectionString =
