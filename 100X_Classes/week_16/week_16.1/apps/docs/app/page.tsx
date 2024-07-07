0a3b28cf3c74f0de05e56bc5685c882d32cd84d9
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-07-07 19:58:02 +0530

    class 16.1 completed

--- 100X_Classes/week_16/week_16.1/apps/docs/app/page.tsx
+++ 100X_Classes/week_16/week_16.1/apps/docs/app/page.tsx
@@ -5,6 +5,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <div> ia m from docs page</div>
       <main className={styles.main}>
         <Image
           className={styles.logo}
