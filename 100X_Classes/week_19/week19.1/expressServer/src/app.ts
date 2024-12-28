8a97e9a403ee3a0e1f2d1745a6b856c5543f483a
saifGit2417 <saifwork2417@gmail.com>
2024-12-28 16:41:27 +0530

    week 19.1 class completed

---
+++ 100X_Classes/week_19/week19.1/expressServer/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { WebSocketServer ,WebSocket} from "ws";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("hello from get api call")
+})
+const httpServer = app.listen(8080);
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data+"this string is added from server web socket", { binary: isBinary });
+      }
+    });
+    console.log("message received" , data)
+  });
+
+  ws.send("Hello! Message From Server!!");
+});
---
+++ 100X_Classes/week_19/week19.1/frontEndWebSocket/src/App.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [msg, setMsg] = useState([""]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const newSocket = new WebSocket("ws://localhost:8080");
+    newSocket.onopen = () => {
+      setSocket(newSocket);
+    };
+    newSocket.onmessage = (message) => {
+      console.log("Message received:", message);
+      console.log("Message received:", message.data);
+      setMsg((prev) => [...prev, message.data]);
+    };
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, []);
+
+  // same logic will be there in next js as well only make sure to make this as client component
+  return (
+    <>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => socket?.send(input)}>Send message</button>
+      {msg.map((data) => {
+        return <h1>{data} </h1>;
+      })}
+    </>
+  );
+}
+
+export default App;
